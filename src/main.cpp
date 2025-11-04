f92dd9ea6e59af77be2a2fe5b0a9100b3c9eacb5
arinsingh185 <arinsingh185@gmail.com>
2025-11-04 08:29:24 -0700

    Made the square now I need to do the excersizes and take notes on everything after the graphics pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,7 @@ int main(){
 		unsigned int fragmentShader;
 		unsigned int shaderProgram;
 		unsigned int VAO;
+		unsigned int EBO;
 		//initialie glfw and set the version and the render api being used
 		glfwInit();
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -59,16 +60,6 @@ int main(){
 
 
 
-		float vertices[] = {
-			-0.5f, -0.5f, 0.5f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
-		};
-		
-		glGenBuffers(1, &VBO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
 		vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
 		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -89,28 +80,43 @@ int main(){
 		glDeleteShader(vertexShader);
 		glDeleteShader(fragmentShader);
 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
-
-
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
-
 		glUseProgram(shaderProgram);
 
-		glGenVertexArrays(1, &VAO);
 
+
+
+		float vertices[] = {
+			0.5f, 0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f,
+			-0.5f, -0.5f, 0.0f,
+			-0.5f, 0.5f, 0.0f
+		};
+
+		unsigned int indices[] = {
+			0, 1, 3,
+			1, 2, 3
+		};
+		
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+		glGenVertexArrays(1, &VAO);
 
 		glBindVertexArray(VAO);
 
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+
+		
+
 
 		//render loop
 		while (!glfwWindowShouldClose(window)) {
@@ -125,7 +131,8 @@ int main(){
 
 			glUseProgram(shaderProgram);
 			glBindVertexArray(VAO);
-			glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
 
 			//check and call events and then swap the buffer
 			glfwPollEvents();
